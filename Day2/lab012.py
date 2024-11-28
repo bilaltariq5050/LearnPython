88035626ce11c01c51452d3e332eda2f62834900
Bilal Tariq <bilaltariq5050@gmail.com>
2024-11-28 12:55:56 +0500

    added

---
+++ Day2/lab012.py
@@ -0,0 +1,6 @@
+x=5 #int
+pi=3.14 #float
+name="bilal" #string
+
+a,b,c=1,2,3
+print(a,b,c)
---
+++ Day2/practice.py
@@ -0,0 +1,5 @@
+import calendar
+yy=2024
+mm=11
+print(calendar.month(yy,mm))
+print(calendar.calendar(yy))
